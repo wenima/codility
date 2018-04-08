37d7e62ea427c43d8e399802fd91b484c0b1b385
MarcMBPDev <marc.wenicker@gmail.com>
2018-04-08 15:34:53 -0700

    initial commit

---
+++ junior_dev_ops/binary_tree.py
@@ -0,0 +1,5 @@
+"""Solution for Quesiton Three of the Junior DevOps Codility Test."""
+
+def solution(T):
+    """Return the number of minimum unique value in any path from root to leaf."""
+    pass
